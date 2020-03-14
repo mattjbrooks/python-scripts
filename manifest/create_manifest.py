a9662479a12162bf2c57089929a1974a2d9087f6
mattjbrooks <mbrooks.github@gmail.com>
2020-03-14 00:59:01 +0000

    Use default values when creating manifest object

--- manifest/create_manifest.py
+++ manifest/create_manifest.py
@@ -1,10 +1,7 @@
 #! /usr/bin/python3
 
-import datetime
 import os
 from manifest import Manifest
 
-date = datetime.datetime.now().strftime('%Y-%m-%d')
-manifest_filename = f"{date}.json"
-manifest = Manifest(os.getcwd(), manifest_filename)
+manifest = Manifest()
 manifest.create_manifest()
