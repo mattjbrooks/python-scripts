26f244990eafdb5507fa35b19c45f6a155744522
mattjbrooks <mbrooks.github@gmail.com>
2020-03-18 11:35:33 +0000

    Remove now unneeded files

--- manifest/check_manifest.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-
-import os
-import parser
-from manifest import Report
-
-date = parser.check_date()
-report = Report(manifest_filename=f"{date}.json")
-print(report.log(color=True))
--- manifest/create_manifest.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python3
-
-import os
-from manifest import Manifest
-
-manifest = Manifest()
-manifest.create_manifest()
