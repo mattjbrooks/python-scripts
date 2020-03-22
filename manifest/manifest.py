3f6b8e0adda7b4081b3299f5bb4953528c1792a0
mattjbrooks <mbrooks.github@gmail.com>
2020-03-22 16:29:21 +0000

    Use repr conversion flag in Report

--- manifest/manifest.py
+++ manifest/manifest.py
@@ -98,7 +98,7 @@ class Report():
 
     def __repr__(self):
         return (f"{self.__class__.__name__}(" +
-                ", ".join([f"'{key}={value}'" 
+                ", ".join([f"{key}={value!r}" 
                     for key, value in self.__dict__.items()]) + ")"
         )
 
